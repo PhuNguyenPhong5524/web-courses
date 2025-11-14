56e75df8153596038556232dc7d33206714578fb
nguyenphongphu1234 <134608320+PhuNguyenPhong5524@users.noreply.github.com>
2025-11-14 10:31:27 +0700

    update test 19

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -1,15 +1,22 @@
-// Header
+function getBasePath() {
+    const path = window.location.pathname;
+    if (path.includes("/page/")) {
+        return "/layout/";
+    }
+    return "layout/";
+}
+
 async function showHeader() {
-    const res = await fetch("/layout/header.html");
+    const res = await fetch(getBasePath() + "header.html");
     const headerHtml = await res.text();
     document.querySelector(".showHeader").innerHTML = headerHtml;
 }
-showHeader();
 
-// Footer
 async function showFooter() {
-    const res = await fetch("/layout/footer.html");
+    const res = await fetch(getBasePath() + "footer.html");
     const footerHtml = await res.text();
     document.querySelector(".showFooter").innerHTML = footerHtml;
 }
+
+showHeader();
 showFooter();
