fb7ad9de73625e157e0f5a7234efbceca5d9c86c
nguyenphongphu1234 <134608320+PhuNguyenPhong5524@users.noreply.github.com>
2025-11-27 19:06:25 +0700

    update

--- js/handleClick.js
+++ js/handleClick.js
@@ -1,10 +1,12 @@
 export const handleClickCategory = (e) => {
     const id = e.currentTarget.dataset.id;
     localStorage.setItem("cate_id", id);
-    window.location.href = "../page/user/courses-category.html";
+    window.location.href = "page/user/courses-category.html";
 }
 export const handleClickCourse = (e) => {
     const id = e.currentTarget.dataset.id;
     localStorage.setItem("course_id", id);
-    window.location.href = "../page/user/courses-detail.html";
+    window.location.href = "page/user/courses-detail.html";
 }
+
+
